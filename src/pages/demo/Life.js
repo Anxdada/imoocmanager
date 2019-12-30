ad1b0a973c4d8fbca825993ef42c72720a93263b
Anxdada <xdadaxie@qq.com>
2019-12-31 00:14:26 +0800

    first

---
+++ src/pages/demo/Life.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Child from './Child'
+import './index.less'
+
+export default class Life extends React.Component {
+    constructor(prpos) {
+        super(prpos);
+        this.state = {
+            count: 0
+        };
+    }
+
+    handleAdd = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    handleClick() {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+
+    render() {
+        let style = {
+            padding: 10,
+        };
+        return <div className="content">
+            <p>React 生命周期介绍</p>
+            <button onClick={this.handleAdd}>点击一下</button>
+            <button onClick={this.handleClick.bind(this)}>点击一下</button>
+            <p>{this.state.count}</p>
+            <Child name={this.state.count}></Child>
+        </div>
+    };
+}
